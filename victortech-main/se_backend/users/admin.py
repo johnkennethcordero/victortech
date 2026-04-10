2c0a13ca32d36a32c3e7914083649977219abadb
johnkennethcordero <you@example.com>
2026-04-10 21:25:54 +0800

    fix user admin password hashing

--- victortech-main/se_backend/users/admin.py
+++ victortech-main/se_backend/users/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['id', 'email', 'role', 'is_active', 'is_staff']
+    ordering = ['id']
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role'),
+        }),
+    )
+    search_fields = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
